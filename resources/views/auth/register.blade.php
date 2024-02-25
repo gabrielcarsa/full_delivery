214c243536406aedc41436da586c77d69053fe40
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2024-02-25 13:57:11 -0500

    refactor: delecao de codigos css desnecessarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,7 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Cadastrar Usuário') }}
-        </h2>
-    </x-slot>
     <x-authentication-card>
-        <x-validation-errors class="mb-4" />
+
+        <x-validation-errors class="" />
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
@@ -56,11 +52,6 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
                 <x-button class="ms-4">
                     {{ __('Register') }}
                 </x-button>
