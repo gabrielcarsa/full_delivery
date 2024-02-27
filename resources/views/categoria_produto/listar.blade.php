0ff914985ea43e89d9c0b793d533694fb1fd6553
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2024-02-27 09:50:02 -0500

    refactor: melhoria estilo listagem

--- resources/views/categoria_produto/listar.blade.php
+++ resources/views/categoria_produto/listar.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <!-- Dropdown Card -->
+    <!-- Card Consulta -->
     <div class="card mb-4 mt-4">
         <!-- Card Header  -->
         <div class="card-header py-2 d-flex flex-row align-items-center justify-content-between">
@@ -24,28 +24,49 @@
         </div>
     </div>
 
+    <!-- Card Consulta -->
     @if(isset($categorias))
-    <table class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Descrição</th>
-                <th scope="col">Ordem de exibição</th>
-                <th scope="col">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($categorias as $categoria)
-            <tr>
-                <th scope="row">{{$categoria->id}}</th>
-                <td>{{$categoria->nome}}</td>
-                <td>{{$categoria->descricao}}</td>
-                <td>{{$categoria->ordem}}</td>
-                <td><a href="editar/{{$categoria->id}}" class="btn-acao-listagem-secundary">Alterar</a></td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="card mb-4 mt-4">
+        <!-- Card Header  -->
+        <div class="card-header py-2 d-flex flex-row align-items-center justify-content-between dropdown">
+            <button class="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Exportar
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">PDF</a></li>
+            </ul>
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col">Ordem de exibição</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categorias as $categoria)
+                    <tr>
+                        <th scope="row">{{$categoria->id}}</th>
+                        <td><a class="btn btn-outline-primary" href="">{{$categoria->nome}}</a></td>
+                        <td>{{$categoria->descricao}}</td>
+                        <td>{{$categoria->ordem}}</td>
+                        <td>
+                            <a href="editar/{{$categoria->id}}" class="acoes-listar btn-acao-listagem-secundary"><i
+                                    class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="editar/{{$categoria->id}}" class="acoes-listar btn-acao-listagem-secundary text-danger"><i
+                                    class="fa-solid fa-trash"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
     @endif
 </x-app-layout>
