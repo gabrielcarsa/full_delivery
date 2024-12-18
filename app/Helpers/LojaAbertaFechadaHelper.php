3857bf1f8d40f773d283d6a2cced262215583169
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2024-12-18 16:08:32 -0500

    feat: integrando Loja Aberta e Fechado com iFood

--- app/Helpers/LojaAbertaFechadaHelper.php
+++ app/Helpers/LojaAbertaFechadaHelper.php
@@ -3,19 +3,49 @@
 namespace App\Helpers;
 
 use App\Models\Loja;
+use App\Services\IfoodService;
 
 class LojaAbertaFechadaHelper
 {
     public static function getLojaStatus(){
+
+        //instancindo IfoodService
+        $ifoodService = new IfoodService();
+
         //Se houver uma loja conectada
         if(session('lojaConectado')){
+            
+            //Obtendo Loja
             $loja = Loja::where('id', session('lojaConectado')['id'])->first();
 
-            //Verificar se loja está aberta 
-            if($loja->is_open == true){
-                return true;
+            //Se houver integração com iFood
+            if($loja->ifood_merchant_id != null){
+
+                $merchant = $ifoodService->getMerchant($loja->ifood_merchant_id);
+
+                //Verificando se está aberto no iFood
+                if($merchant['status'] == 'AVAILABLE'){
+                    
+                    $loja->is_open = true;
+                    $loja->save();
+
+                    return true;
+                }else{
+
+                    $loja->is_open = false;
+                    $loja->save();
+
+                    return false;
+                }
+
             }else{
-                return false;
+
+                //Verificar se loja está aberta 
+                if($loja->is_open == true){
+                    return true;
+                }else{
+                    return false;
+                }
             }
         }
         //Se não houver loja conectada
