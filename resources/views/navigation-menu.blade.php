0ab3477cedc9ac4a1f33231f33b6448c8bc703c2
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2025-01-09 10:54:36 -0500

    feat: mudando navigation-menu para bootstrap

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,176 +1,90 @@
-<nav x-data="{ open: false }" class="bg-white border-bottom">
-    
-    <!-- Primary Navigation Menu -->
-    <div class=" mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items">
-                    <a href="{{ route('dashboard') }}"
-                        class="d-flex align-items-center text-decoration-none text-dark fs-2 fw-bolder">
-                        <img src="{{asset("storage/images/logo-black.png")}}" width="150px" alt="Foomy">
-                    </a>
-                </div>
-            </div>
-
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <!-- Loja aberta ou fechada -->
-                <div class="ms-3 relative">
-                    @if(session('lojaConectado') != null)
-                    @if(LojaAbertaFechadaHelper::getLojaStatus() == true)
-                    <div class="d-flex bg-light p-2 rounded">
-                        <p class="m-0 d-flex align-items-center text-success">
-                            <span class="material-symbols-outlined mr-1" style="font-variation-settings: 'FILL' 1;">
-                                circle
+<nav class="navbar navbar-expand-lg bg-white border-bottom">
+    <div class="container-fluid">
+        <!-- Logo -->
+        <div class="shrink-0 flex items">
+            <a href="{{ route('dashboard') }}"
+                class="d-flex align-items-center text-decoration-none text-dark fs-2 fw-bolder">
+                <img src="{{asset("storage/images/logo-black.png")}}" width="150px" alt="Foomy">
+            </a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item mx-1">
+                    <!-- Loja aberta ou fechada -->
+                    <div class="ms-3 relative">
+                        @if(session('lojaConectado') != null)
+                        @if(LojaAbertaFechadaHelper::getLojaStatus() == true)
+                        <div class="d-flex bg-light p-2 rounded">
+                            <p class="m-0 d-flex align-items-center text-success">
+                                <span class="material-symbols-outlined mr-1" style="font-variation-settings: 'FILL' 1;">
+                                    circle
+                                </span>
+                                Loja Aberta
+                            </p>
+                            <span class="fs-6 mx-2">
+                                -
                             </span>
-                            Loja Aberta
-                        </p>
-                        <span class="fs-6 mx-2">
-                            -
-                        </span>
-                        <button onClick="window.location.reload()" class="text-primary text-decoration-underline">
-                            Recarregar
-                        </button>
-                    </div>
-                    @elseif(LojaAbertaFechadaHelper::getLojaStatus() == false)
-                    <div class="d-flex bg-light p-2 rounded">
-                        <p class="m-0 d-flex align-items-center text-danger">
-                            <span class="material-symbols-outlined mr-1 " style="font-variation-settings: 'FILL' 1;">
-                                circle
-                            </span>
-                            Loja Fechada
-                        </p>
-                        <span class="fs-6 mx-2">
-                            -
-                        </span>
-                        <button onClick="window.location.reload()" class="text-primary text-decoration-underline">
-                            Recarregar
-                        </button>
-                    </div>
-                    @endif
-                    @endif
-                </div>
-
-                <!-- Settings Dropdown -->
-                <div class="ms-3 relative">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                            <button
-                                class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                <img class="h-8 w-8 rounded-full object-cover"
-                                    src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                            <button onClick="window.location.reload()" class="text-primary text-decoration-underline">
+                                Recarregar
                             </button>
-                            @else
-                            <span class="inline-flex rounded-md">
-                                <button type="button"
-                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                    {{ Auth::user()->name }}
-
-                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                    </svg>
-                                </button>
+                        </div>
+                        @elseif(LojaAbertaFechadaHelper::getLojaStatus() == false)
+                        <div class="d-flex bg-light p-2 rounded">
+                            <p class="m-0 d-flex align-items-center text-danger">
+                                <span class="material-symbols-outlined mr-1 "
+                                    style="font-variation-settings: 'FILL' 1;">
+                                    circle
+                                </span>
+                                Loja Fechada
+                            </p>
+                            <span class="fs-6 mx-2">
+                                -
                             </span>
-                            @endif
-                        </x-slot>
+                            <button onClick="window.location.reload()" class="text-primary text-decoration-underline">
+                                Recarregar
+                            </button>
+                        </div>
+                        @endif
+                        @endif
+                    </div>
+                </li>
+                <li class="nav-item mx-1">
+                    <div class="dropdown">
+                        <a class="btn text-secondary border rounded dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
 
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-
-                            <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item" href="{{ route('profile.show') }}">
+                                    Perfil
+                                </a>
+                            </li>
+                            <li>
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}" x-data>
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">
+                                        {{ __('Log Out') }}
+                                    </button>
+                                </form>
+                            </li>
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                {{ __('API Tokens') }}
-                            </x-dropdown-link>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('api-tokens.index') }}">
+                                    {{ __('API Tokens') }}
+                                </a>
+                            </li>
                             @endif
-
-                            <div class="border-t border-gray-200"></div>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
-
-                                <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="flex items-center px-4">
-                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                <div class="shrink-0 me-3">
-                    <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
-                        alt="{{ Auth::user()->name }}" />
-                </div>
-                @endif
-
-                <div>
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                </div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Account Management -->
-                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                <x-responsive-nav-link href="{{ route('api-tokens.index') }}"
-                    :active="request()->routeIs('api-tokens.index')">
-                    {{ __('API Tokens') }}
-                </x-responsive-nav-link>
-                @endif
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-
-                    <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-
-            </div>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
